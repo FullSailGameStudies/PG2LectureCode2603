5c7097608bf8b0f6d83f0bfc19706a20586d73f3
Garrett Girod <girodg@hotmail.com>
2026-03-13 12:14:42 -0400

    day4, day5-ish

--- Week2/Lectures/Lectures.cpp
+++ Week2/Lectures/Lectures.cpp
@@ -4,11 +4,58 @@
 #include "Day5.h"
 #include "Day6.h"
 #include <Input.h>
+#include <map>
 
+//only use recursive methods for DETERMINANT loops
+//(you know how many times it's going to loop)
+void recursion(int n)
+{
+	//2) an exit condition (a base case)
+	if(n < 10)
+	{
+		int b = 5;
+		//1) a recursive call
+		std::cout << n << " ";
+		recursion(n + 1);
+		Console::Write(n, (ConsoleColor)(rand() % 7));
+	}
+}
 
 int main(int argc, char* args[])
 {
+	recursion(0);
 	srand(static_cast<unsigned int>(time(NULL)));
+
+	std::map<std::string, float> menu;
+	//1) easy way
+	//	  map[key] = value;
+	menu["hotdogs"] = 1.99f;
+	menu["lamb"] = 14.99f;
+	menu["chicken pot pie"] = 7.99f;
+	menu["Dino Nuggies"] = 12.99f;
+	menu["Dino Nuggies"] = 14.99f;//overwrite the value
+	//2) not-easy way
+	//	 map.insert(key-valuepair);
+	std::pair< std::string, float> menuPair =
+		std::make_pair("burger", 8.99f);
+	menu.insert(menuPair);
+	menuPair.second = 11.99f;
+	auto wasInserted =  menu.insert(menuPair);//does NOT overwrite the value
+
+	std::cout << "\n\nMenu: \n";
+	for (auto& pair : menu)
+	{
+		std::cout << pair.first << " " << pair.second << "\n";
+	}
+	//structured bindings
+	for (auto& [menuItem,menuPrice] : menu)
+	{
+		std::cout << menuItem << " " << menuPrice << "\n";
+	}
+	for (auto it = menu.begin(); it != menu.end(); it++)
+	{
+		std::cout << it->first << " " << it->second << "\n";
+	}
 
 	std::string hello = "Hello Week 2!";
 	for (auto& ch : hello)
--- Week2/Lectures/Sorter.cpp
+++ Week2/Lectures/Sorter.cpp
@@ -4,3 +4,48 @@
 //
 // Part A-2
 //
+/*
+		procedure bubbleSort(A : list of sortable items)
+		  n := length(A)
+		  repeat
+			  swapped := false
+			  for i := 1 to n - 1 inclusive do
+				  if A[i - 1] > A[i] then
+					  swap(A, i - 1, i)
+					  swapped = true
+				  end if
+			  end for
+			  n := n - 1
+		  while swapped
+		end procedure
+*/
+void Sorter::bubbleSort(std::vector<Light>& A)
+{
+	//n := length(A)
+	int n = A.size();
+	bool swapped;
+	do
+	{
+		swapped = false;
+		//for i := 1 to n - 1 inclusive do
+		for (int i = 1; i <= n - 1; i++)
+		{
+			//if A[i - 1] > A[i] then
+			if (A[i - 1].red > A[i].red)
+			{
+				//TODO: figure out how to swap!
+				//swap(A, i - 1, i)
+				std::swap(A[i - 1], A[i]);
+				//OG swap
+				//Light l1 = A[i - 1];
+				//A[i - 1] = A[i];
+				//A[i] = l1;
+
+				//swapped = true
+				swapped = true;
+			}//endif
+		}//endfor
+		//n := n - 1
+		--n;
+	} while (swapped);
+}//end procedure
