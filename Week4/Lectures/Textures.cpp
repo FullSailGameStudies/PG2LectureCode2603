411fb9903f57e70beb6326239b566d82966290af
Garrett Girod <girodg@hotmail.com>
2026-03-23 12:04:58 -0400

    day 10: part A-1

--- Week4/Lectures/Textures.cpp
+++ Week4/Lectures/Textures.cpp
@@ -8,7 +8,47 @@ bool Textures::LoadSubtextures()
 	//
 	// Part A-1
 	//
+	std::string fileName = "character_robot_sheetHD.csv";
+	std::ifstream inFile(fileName);
+	if (inFile.is_open())
+	{
+		char colDelim = '?';
+		std::getline(inFile, imageFile_);
+		while (not inFile.eof())
+		{
+			std::string line;
+			std::getline(inFile, line);
 
+			SubTexture sub;
+			std::stringstream lineStream(line);
+			std::getline(lineStream, sub.name, colDelim);
+
+			std::string numData;
+			std::getline(lineStream, numData, colDelim);
+			sub.x = std::stoi(numData);
+
+			std::getline(lineStream, numData, colDelim);
+			sub.y = std::stoi(numData);
+
+			std::getline(lineStream, numData, colDelim);
+			sub.width = std::stoi(numData);
+
+			std::getline(lineStream, numData, colDelim);
+			sub.height = std::stoi(numData);
+
+			maxWidth_ = std::max(maxWidth_, sub.width);
+			maxHeight_ = std::max(maxHeight_, sub.height);
+
+			subTextures_.push_back(sub);
+		}
+		inFile.close();
+		return true;
+	}
+	else
+	{
+		std::cout << fileName << " could not be opened.\n";
+	}
+	inFile.close();
 	return false;
 }
 
